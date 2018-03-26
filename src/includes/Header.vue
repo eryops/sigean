31328cc18f850ff9d6b76a8e0732e3e3b6842ece
Johanna Liljecrantz <johanna.liljecrantz@creo.se>
2018-03-26 21:12:13 +0200

    Fix menu and start fix on hero.

--- src/includes/Header.vue
+++ src/includes/Header.vue
@@ -1,5 +1,58 @@
 <template>
-  <a href="#app" title="Till sidans topp">
-    <h1>Hyr hus i Sigean, en fransk medeltidsstad</h1>
-  </a>
+  <header class="page-header">
+    <button type="button"
+            class="mobile-menu-button"
+            title="Öppna menyn"
+            @click="mobileMenuOpend = !mobileMenuOpend">
+      <span class="icon-menu-opend" v-if="mobileMenuOpend">Öppen</span>
+      <span class="icon-menu-closed" v-else>Stängd</span>
+    </button>
+    <nav :class="mobileMenuOpend ? 'mobile-menu-opend' : 'mobile-menu-closed'" class="content">
+      <a href="#app"
+         v-scroll-to="'#app'"
+         @click="mobileMenuOpend = !mobileMenuOpend">
+        Start
+      </a>
+      <a href="#about"
+         v-scroll-to="'#about'"
+         @click="mobileMenuOpend = !mobileMenuOpend">
+        Om huset
+      </a>
+      <a href="#imgHouse"
+         v-scroll-to="'#imgHouse'"
+         @click="mobileMenuOpend = !mobileMenuOpend">
+        Foton på huset
+      </a>
+      <a href="#imgSurandings"
+         v-scroll-to="'imgSurandings'"
+         @click="mobileMenuOpend = !mobileMenuOpend">
+        Foton på omgivning
+      </a>
+      <a href="#map"
+         v-scroll-to="'#map'"
+         @click="mobileMenuOpend = !mobileMenuOpend">
+        Karta
+      </a>
+    </nav>
+  </header>
 </template>
+<script>
+  export default {
+    name: 'Header',
+    data() {
+			return {
+				mobileMenuOpend: false
+			}
+		},
+    methods: {
+      toggleMenu() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+      }
+    }
+  }
+</script>
--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="hero" class="bg-img-cover bg-fixed">
+  <div id="hero" class="bg-img-cover bg-fixed bg-hero">
     <div class="content">
+      <h1>Hyr hus i Sigean</h1>
       <span class="chevron-down bounce"></span>
     </div>
   </div>
