7172a3157d5a1a944bc7a8d18d9c30dd89369d8e
Johanna Liljecrantz <johanna.liljecrantz@creo.se>
2018-09-04 22:33:17 +0200

    Update about

--- src/components/AboutText.vue
+++ src/components/AboutText.vue
@@ -1,11 +1,20 @@
 <template>
   <div :class="visible ? 'slide-in-top': 'slide-out-top'">
-    <h2 class="text-center">Who am I?</h2>
-    <p class="text-center">
-      I'm a Front-End Developer livning whit my family in Åkersberga,<br>
-      north of Stockholm. <br>
-      I have a passion for accessibility and making the web accessible for all. <br>
-      For me the perfect balance between job and family is working four days a week, I get more time whit my family and I have a better focus and energy on my job when I worke.
+    <h2 class="text-center">Vårt franska medelhavshus</h2>
+    <p>
+      Vi har köpt ett underbart gammalt hus i hjärtat av en fransk medeltidsstad. Huset ligger i ett kvarter med trånga charmiga gränder. Det fyra våningar höga huset har en innergård och en terrass med mycket sol. Huset består av tre sovrum, allrum, kök med stor matplats, tvättstuga och badrum med separat toalett. Staden heter Sigean och ligger i regionen Languedoc vid medelhavet nära spanska gränsen. Inom en mil finns underbara badstränder med och utan restauranger intill, vingårdar och en härlig natur.
+    </p>
+    <p>
+      I stadens centrum, inom en radie av 200 meter från huset, finns flera bistron, restauranger, pizzerior, mataffärer, bagerier och andra butiker. Två förmiddagar i veckan, på tisdagar och fredagar, är det torghandel där man kan hitta allt från färsk frukt och grönsaker till kläder.
+    </p>
+    <p>
+      I utkanten av staden finns all annan tänkbar service.
+    </p>
+    <p>
+      Sigean ligger cirka fem kilometer från kustmotorvägen, så det är lätt att hitta dit.
+    </p>
+    <p>
+      Sigean är ett underbart ställe för avkoppling och nöje med närhet till ett underbart landskap med sevärdheter, bad och mycket annat.
     </p>
   </div>
 </template>
