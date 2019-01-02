c7e9ba837b81d38e0c139d85a1a65c4ab54a0240
Johanna Liljecrantz <johanna.liljecrantz@creo.se>
2019-01-02 23:01:59 +0100

    Fix image componet

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,15 +1,129 @@
 <template>
-  <div id="">
-
-  </div>
+	<div :class="{ 'flipper': imgText }" v-if="bgImg">
+		<div :class="[{ 'flipper-card': imgText }, 'image-component-container', imgRatio]">
+			<div :class="[bgImg, { 'front': imgText },'image-component']">
+				<h3 v-if="imgTitle" :class="[color + '-before-corner', imgTitlePos]">{{ imgTitle }}</h3>
+			</div>
+			<div v-if="imgText" :class="['image-component back', color + '-bg']">
+				<p>{{ imgText }}</p>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  name: "",
-  data: () => ({
-
-  })
+	props: {
+		bgImg: {
+			default: () => false
+		},
+		imgRatio: {
+			default: () => 'ratio-16-9'
+		},
+		imgTitle: {
+			default: () => false
+		},
+		imgTitlePos: {
+			default: () => 'tl'
+		},
+		imgText: {
+			default: () => false
+		},
+		color: {
+			default: () => 'blue'
+		}
+	}
 }
 </script>
-<style lang="scss" scoped>
+
+<style lang="scss">
+@import "~bootstrap/scss/functions";
+
+@import '../assets/sass/variables';
+
+@import "~bootstrap/scss/mixins/breakpoints";
+@import "~bootstrap/scss/mixins/grid-framework";
+@import "~bootstrap/scss/mixins/grid";
+@import "~bootstrap/scss/grid";
+
+.image {
+	&-component {
+		width: 100%;
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: bottom;
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		&-container {
+			position: relative;
+			margin: 1rem 0;
+		}
+		h3 {
+			margin: 0;
+			position: absolute;
+			z-index: 1;
+			&:before {
+				content: '';
+				position: absolute;
+				z-index: -1;
+				border-style: solid;
+			}
+			&.tl {
+				top: 0;
+				left: 0;
+				padding-left: 0.5rem;
+				&:before {
+					top: 0;
+					left: 0;
+					border-width: 15vw 66vw 0 0;
+					@include media-breakpoint-up(sm) {
+						border-width: 4.5rem 20rem 0 0;
+					}
+				}
+			}
+			&.tr {
+				top: 0;
+				right: 0;
+				padding-right: 0.5rem;
+				&:before {
+					right: 0;
+					top: 0;
+					border-width: 0 66vw 15vw 0;
+					@include media-breakpoint-up(sm) {
+						border-width: 0 20rem 4.5rem 0;
+					}
+				}
+			}
+			&.br {
+				bottom: 0;
+				right: 0;
+				padding-right: 0.5rem;
+				&:before {
+					bottom: 0;
+					right: 0;
+					border-width: 0 0 15vw 66vw;
+					@include media-breakpoint-up(sm) {
+						border-width: 0 0 4.5rem 20rem;
+					}
+				}
+			}
+			&.bl {
+				bottom: 0;
+				left: 0;
+				padding-left: 0.5rem;
+				&:before {
+					bottom: 0;
+					left: 0;
+					border-width: 15vw 0 0 66vw;
+					@include media-breakpoint-up(sm) {
+						border-width: 4.5rem 0 0 20rem;
+					}
+				}
+			}
+		}
+		p {
+			padding: 1rem;
+		}
+	}
+}
 </style>
--- src/sections/House.vue
+++ src/sections/House.vue
@@ -2,16 +2,28 @@
 	<div id="house">
 		<div class="container">
 			<b-row>
-				<b-col cols="12" sm="6" lg="4">
-					
+				<b-col cols="12" sm="6">
+					<c-image 	bgImg="bg-img-component-terrace"
+								imgRatio="ratio-8-9"
+								imgTitle="Taktreass"
+								imgText="Landjaeger ham pork belly, kielbasa pork chop sirloin cow filet mignon cupim shankle short loin chuck chicken."/>
 				</b-col>
-				<b-col cols="12" sm="6" lg="4">
-
+				<b-col cols="12" sm="6">
+					<c-image 	bgImg="bg-img-component-terrace"
+								imgRatio="ratio-8-9"
+								imgTitle="Taktreass"
+								imgTitlePos="tr"/>
 				</b-col>
 			</b-row>
 		</div>
 	</div>
 </template>
 <script>
+import CImage from '@/components/Image.vue'
 
+export default {
+	components: {
+		CImage
+	}
+}
 </script>
